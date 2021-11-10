2772ae32dd7251831a63f5327052bdebf8e0339e
MATEUS_LOUBACH <MCESARLOUBACH@GMAIL.COM>
2021-11-10 10:03:45 -0300

    nova chamada home

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 function Home() {
     return (
         <div>
-            <h2>Página Inicial</h2>
+            <h2>Página Inicial!</h2>
             <Contador />
         </div>
     )
